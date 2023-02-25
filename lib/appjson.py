bafb0852b2e8a71dca67529605ab42b8877efbab
c01952f666540050efb7f2750d8e3e91 <felix.voo@ledger.com>
2023-02-25 13:40:30 +0100

    adding to appjson class

--- lib/appjson.py
+++ lib/appjson.py
@@ -14,6 +14,17 @@ class appjson:
             }
         }
 
+    # Just add an account
+    def add_account(self, account_object:dict):
+        # Append account to account list
+        self.file['data']['accounts'].append(account_object)
+
+    def return_accountnumber(self):
+        return len(self.file['data']['accounts'])
+
+    def return_appjson(self):
+        return self.file
+
     def test_ret_appjson(self, account_object:dict):
         # print(account_object)
         self.file['data']['accounts'].append(account_object)
