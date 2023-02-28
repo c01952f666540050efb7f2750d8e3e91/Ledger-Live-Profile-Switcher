23f80ba7b847efd2962c72f86d3eaf6e27bf3331
c01952f666540050efb7f2750d8e3e91 <felix.voo@ledger.com>
2023-02-28 22:53:45 +0100

    adding cli functionality

--- test.py
+++ test.py
@@ -1,18 +1,80 @@
 # Imports
-import os
-import shutil
-from pprint import pprint
-import json
+import argparse
 
 # Internal Import
-from lib.accounts import accounts
+from lib.accounts import accounts, account_types
 from lib.appjson import appjson
 from lib.writer import inject_appjson, replace_appjson
 
-# We shall use this as the testing file in the future
+# We shall use this as the testing file eventually
+
+# How should we use this CLI tool
+
+# Create parser object
+parser = argparse.ArgumentParser(
+    description="Inject addresses into app.json of Ledger Live"
+)
+parser.add_argument(
+    "--verbose",
+    action="store_true",
+    help="Boolean for verbosity for this tool for testing purposes"
+) # Verbosity
+
+# We first parse all the arguments for the options that we will have
+# Create argument object
+args = parser.parse_args()
+
+# Let the user know we are being verbose
+if args.verbose:
+    print("Beginnging script in verbose mode.")
+
+# We then ask the user to enter the account types and "public keys" (eg addresses or xpubs) in a loop
+
+# Exit boolean
+continue_loop = True
+
+# Added account list
+added_accounts = []
+
+# Begin loop
+while continue_loop:
+
+    # If this is the first run
+    if len(added_accounts) == 0:
+        # Print account types:
+        print(account_types)
+    else:
+        print("Exit: (x) / Back: (b)")
+
+    # Get address type input
+    typed_input = input("What address type do you want to add? ")
+
+    # Check for exit or back inputs
+    if typed_input == 'b':
+        pass
+    elif typed_input == 'x':
+        raise(SystemExit())
+
+    # Check address type is supported
+    if typed_input in account_types:
+        print(f"Account type blockchain!")
+
+    
+
+exit()
+
+
+
+
+# Once we have everything we need, we print a confirmation with all of the addresses that we want to add in a table
+
+# We input a selection mechanism to remove addresses if need be
+
+# Confirm and write to file
+
 
 # We have a specific address we want to inject
-test_address = "0x777fDB494d0825669Bb50f5B1e075E18e671F8A7"
+test_address = "0xbBEFA5eE210719D21AEAc41B0722C19624d32A5B"
 test_obj = appjson()
 
 # This creates the account
